fa50f5e18528ff88227f4665367608881d1fe73e
Gamer153 <die.experimenter12@gmail.com>
2022-05-28 21:18:38 +0200

    Start Kotlin project
    Signed-off-by: Gamer153 <die.experimenter12@gmail.com>

---
+++ Kotlin/app/bin/main/GuessingGameKotlin/App.kt
@@ -0,0 +1,12 @@
+package GuessingGameKotlin
+
+import java.util.Scanner
+
+val scanner = Scanner(System.`in`)
+
+fun getNr() = scanner.nextInt()
+
+fun main() {
+    var number = 0
+    var input = 0
+}
